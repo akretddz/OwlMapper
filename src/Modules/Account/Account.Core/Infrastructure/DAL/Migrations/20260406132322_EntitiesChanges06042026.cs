2e0b8af5adfb3bcdaec51ec236937adbb9037f2c
akret <aleksander.kret@mirjan24.com>
2026-04-06 15:27:56 +0200

    new migration

---
+++ src/Modules/Account/Account.Core/Infrastructure/DAL/Migrations/20260406132322_EntitiesChanges06042026.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Account.Core.Infrastructure.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class EntitiesChanges06042026 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
